a06194a7cce597e277f866de0e216de52b2e1847
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-05-29 15:25:03 +0100

    update: added all components to home page

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,11 +1,20 @@
 import NavBar from '@/components/NavBar'
+import Alert from '@/components/homecomponents/Alert'
+import Circles from '@/components/homecomponents/Circles'
+import Skills from '@/components/homecomponents/Skills'
+import Stats from '@/components/homecomponents/Stats'
+import Workshops from '@/components/homecomponents/Workshops'
 import React from 'react'
 
 const home = () => {
   return (
     <div>
         <div className='fixed bottom-0'><NavBar /></div>
-        
+        <Alert />
+        <Stats />
+        <Skills />
+        <Workshops />
+        <Circles />
     </div>
   )
 }
