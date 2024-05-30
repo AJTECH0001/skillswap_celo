604fec2ca1a9219c8f484d4bea1e7e16393b29da
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-05-31 00:18:44 +0100

    update: changed maxwidth

--- pages/home.tsx
+++ pages/home.tsx
@@ -8,7 +8,7 @@ import React from 'react'
 
 const home = () => {
   return (
-    <div className='px-3 max-w-sm mx-auto'>
+    <div className='px-3 max-w-md mx-auto'>
         <div className='fixed bottom-0'><NavBar /></div>
         <Alert />
         <Stats />
