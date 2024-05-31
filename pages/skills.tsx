a67ea5aacdb2bd5603188c5d5d730cf706c42973
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-05-31 07:43:41 +0100

    update: done with search

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,8 +1,23 @@
+import NavBar from '@/components/NavBar'
+import Alert from '@/components/homecomponents/Alert'
+import Featured from '@/components/skillscomponent/Featured'
+import Recent from '@/components/skillscomponent/Recent'
+import Search from '@/components/skillscomponent/Search'
 import React from 'react'
 
 const skills = () => {
   return (
-    <div>skills</div>
+    <div className="max-w-md mx-auto ">
+      <div className="fixed bottom-0 w-full z-10">
+        <NavBar />
+      </div>
+      <div className="px-3">
+        <Alert />
+        <Search />
+        <Featured />
+        <Recent />
+      </div>
+    </div>
   )
 }
 
