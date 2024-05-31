ca28ccef0a831f7eacd4533aab44e8019cc25c1d
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-05-31 11:33:49 +0100

    update: done with skill details

--- components/skilldetailcomponent/Request.tsx
+++ components/skilldetailcomponent/Request.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
 
 const Request = () => {
   return (
-    <div>Request</div>
-  )
-}
+    <div>
+      <div>
+        <div className="pt-8 pb-2">
+          <button className="bg-[#6A93FF] w-full text-white h-[44px] rounded-[12px]">
+            Request Service
+          </button>
+        </div>
 
-export default Request
+        <p className="text-xs text-[#666666] text-center">Contact Alice to discuss your logo design needs and start your project.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Request;
