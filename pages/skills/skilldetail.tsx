3e9aa0e6c7d3eca6f6fdd40d189e32fd424583bb
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-05-31 11:02:10 +0100

    update: done with portfolio

--- pages/skills/skilldetail.tsx
+++ pages/skills/skilldetail.tsx
@@ -1,11 +1,14 @@
-import NavBar from '@/components/NavBar'
-import Info from '@/components/skilldetailcomponent/Info'
-import Portfolio from '@/components/skilldetailcomponent/Portfolio'
-import Reviews from '@/components/skilldetailcomponent/Reviews'
-import Similar from '@/components/skilldetailcomponent/Similar'
-import React from 'react'
+import NavBar from "@/components/NavBar";
+import Info from "@/components/skilldetailcomponent/Info";
+import Portfolio from "@/components/skilldetailcomponent/Portfolio";
+import Reviews from "@/components/skilldetailcomponent/Reviews";
+import Similar from "@/components/skilldetailcomponent/Similar";
+import React, { useEffect } from "react";
 
 const skilldetail = () => {
+  useEffect(() => {
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
   return (
     <div className="max-w-md mx-auto ">
       <div className="fixed bottom-0 w-full z-10">
@@ -18,7 +21,7 @@ const skilldetail = () => {
         <Similar />
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default skilldetail
+export default skilldetail;
