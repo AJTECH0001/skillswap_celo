0bc72262293540d4b57a5bbb2dd75552849dc416
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-06-02 17:02:40 +0100

    update: changed the home page

--- components/homecomponents/Alert.tsx
+++ components/homecomponents/Alert.tsx
@@ -1,22 +1,33 @@
-import Image from 'next/image'
-import NotificationIcon from "../../assets/alert/notification.svg"
-import ProfileImg from "../../assets/alert/profileimg.svg"
-import { useEffect } from 'react'
+import Image from "next/image";
+import NotificationIcon from "../../assets/alert/notification.svg";
+import ProfileImg from "../../assets/alert/profileimg.svg";
+import { useEffect } from "react";
 
 const Alert = () => {
   useEffect(() => {
-    return window.scrollTo({ top: 0, behavior: 'smooth' });
-  }, [])
+    return window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
   return (
-    <div className='pt-4  -scroll-mt-96'>
-      <div className='flex justify-end space-x-5'>
-        <Image src={NotificationIcon} alt="notify_" />
-        <div>
-          <Image src={ProfileImg} alt="img_" className='w-[32px] h-[32px] rounded-full' />
+    <div className="pt-4  -scroll-mt-96">
+      <div className="flex justify-between space-x-4">
+        {/* Connect wallet button */}
+        <button className="bg-[#6A93FF] w-full text-white h-[44px] rounded-[12px]">
+          Connect Wallet
+        </button>
+
+        <div className="flex justify-end items-center space-x-5 w-full">
+          <Image src={NotificationIcon} alt="notify_" />
+          <div>
+            <Image
+              src={ProfileImg}
+              alt="img_"
+              className="w-[32px] h-[32px] rounded-full"
+            />
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Alert
+export default Alert;
