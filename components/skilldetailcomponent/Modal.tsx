4d42d8f33a63495a2e42596e536f824c74a3a0b2
Ezeigbo Emmanuel <ezeigboemmanuel2@gmail.com>
2024-06-03 10:03:01 +0100

    fix: home link and maxwidth

--- components/skilldetailcomponent/Modal.tsx
+++ components/skilldetailcomponent/Modal.tsx
@@ -30,7 +30,7 @@ const Modal = ({ isVisible, setShowModal }: Props) => {
     <div
       onClick={handleClose}
       id="wrapper"
-      className="fixed inset-0 bg-black bg-opacity-25 backdrop-blur-sm z-50 flex justify-center items-center"
+      className="fixed max-w-md inset-0 bg-black bg-opacity-25 backdrop-blur-sm z-50 flex justify-center items-center"
     >
       <div className="w-[95%] flex flex-col">
         <button
